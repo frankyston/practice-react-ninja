28e27315c781ca0425f0c6447b2aa8f2265910f3
Frankyston Lins <frankyston@gmail.com>
2017-12-28 08:50:29 -0300

    lesson button component

--- react-components/src/app.js
+++ react-components/src/app.js
@@ -2,11 +2,26 @@
 
 import React from 'react'
 import Title from './title'
+import Paragraph from './paragraph'
+import Square from './square'
+import Button from './button'
+import LikeButton from './like-button'
+
+const arr = ['red', 'blue', 'green']
 
 const App = React.createClass({
   render: function(){
     return (
-      <Title />
+      <div>
+        <Title />
+        <Paragraph />
+        {arr.map((color, index) => (
+          <Square key={index} color={color} />
+        ))}
+
+        <Button >Text Qualquer</Button>
+        <LikeButton></LikeButton>
+      </div>
     )
   }
 })
